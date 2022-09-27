6ddf7ffb273f5ee4e6b221349f80e8d697937c86
racheldelong <105561085+racheldelong@users.noreply.github.com>
2022-09-26 20:39:06 -0400

    added data2, removed data1 knowledge checks

--- check1.py
+++
@@ -1,6 +0,0 @@
-pet_names = ['Izzy', 'Patch', 'Wishbone', 'Gremlin', 'Ginger']
-pet_types = {'dogs' : 1,'cats' : 4}
-dog = ('Izzy', 'treeing walker coonhound', 2009, "white with black spots")
-
-print('Hello world!')
-print("I have a ", dog[1], " named ", pet_names[0], ". I've had her since ", dog[2], ".", sep='')
